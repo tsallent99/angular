2831864dc14ce6a8ae1928cfe8a376950f0d9c9a
tsallent99 <1503075@uab.cat>
2022-10-25 10:19:58 +0200

    learning angular

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `
+    <p>This is a warning, you are in Danger!</p>
+    `
+})
+
+export class WarningAlertComponent{}
